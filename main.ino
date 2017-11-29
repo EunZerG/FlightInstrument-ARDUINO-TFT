8b1917592fda6fd94acd7785b784ec1d76ad9f56
EunZerG <6459784+EunZerG@users.noreply.github.com>
2017-11-29 21:42:14 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -11,16 +11,16 @@
 ILI9341_due tft = ILI9341_due(TFT_CS, TFT_DC, TFT_RST);
 
 //Definicje kolorów
-#define	BLACK           0x0000
-#define	BLUE            0x88E4
-#define	BORDO           0x057C
+#define BLACK           0x0000
+#define BORDO            0x88E4
+#define BLUE           0x057C
 #define WHITE           0xFFFF
 #define YELLOW          0xFFE0
 #define RED             0xF800
 
 //Definicje stałych używanych do obliczeń
-#define REDRAW_DELAY 32 //Minimalny czas odświeżania wyświetlacza
-#define HOR 102
+#define REDRAW_DELAY 64 //Minimalny czas odświeżania wyświetlacza
+#define HOR 100
 #define XC 160 //Współżędna x środka horyzontu
 #define YC 120 //Współżędna y środka horyzontu
 #define ANGLE_INC 1
@@ -40,66 +40,70 @@ unsigned long redrawTime = 0;
 
 void setup()
 {
-	randomSeed(analogRead(0));	//Inicjacja generatora liczb pseudo-losowych
-	//Inicjacja wyświetlacza
-	Serial.begin(9600);
-	tft.begin();
-	tft.setRotation(iliRotation90);	//Rotacja wyswietlacza o 90 stopni
-	tft.setFont(SystemFont5x7);
-	//Inicjacja podstawowego widoku wyświetlacza
-	tft.fillRect(0, 0,  320, 120, BLUE);                     
-	tft.fillRect(0, 120,320, 120, BORDO); 
-		drawHorizon(0, 0);
-		drawInfo();
-	delay(2000);
+  randomSeed(analogRead(0));  //Inicjacja generatora liczb pseudo-losowych
+  //Inicjacja wyświetlacza
+  Serial.begin(9600);
+  tft.begin();
+  tft.setRotation(iliRotation90); //Rotacja wyswietlacza o 90 stopni
+  tft.setFont(SystemFont5x7);
+  //Inicjacja podstawowego widoku wyświetlacza
+  tft.fillRect(0, 0,  320, 120, BLUE);                     
+  tft.fillRect(0, 120,320, 120, BORDO); 
+    drawHorizon(0, 0);
+    drawInfo();
+  delay(500);
+//    testRoll();
+//    testPitch();
+  delay(2000);
 }
 
 void loop()
 {
-	//Wymuszenie regularnego odświeżania wyświetlacza
-	if (millis() > redrawTime)
-	{
-    	redrawTime = millis() + REDRAW_DELAY;
-   	updateHorizon(random(101) - 50, random (101) - 40);
- 	}
+  //Wymuszenie regularnego odświeżania wyświetlacza
+  if (millis() > redrawTime)
+  {
+      redrawTime = millis() + REDRAW_DELAY;
+    updateHorizon(random(70) - 40, random (50) - 20);
+  }
 }
 
 void updateHorizon(int angle, int pitch)
 {
-	bool draw = 1;
-	int delta_pitch = 0;
-	int pitch_error = 0;
-	int delta_roll  = 0;
-	while ((last_pitch != pitch) || (last_roll != angle))
-	{
-	delta_pitch = 0;
-	delta_roll  = 0;
+  bool draw = 1;
+  int delta_pitch = 0;
+  int pitch_error = 0;
+  int delta_roll  = 0;
+  while ((last_pitch != pitch) || (last_roll != angle))
+  {
+  delta_pitch = 0;
+  delta_roll  = 0;
 
-		if (last_pitch < pitch)
-		{
-		delta_pitch = 1;
-		pitch_error = pitch - last_pitch;
-		}
+    if (last_pitch < pitch)
+    {
+    delta_pitch = 1;
+    pitch_error = pitch - last_pitch;
+    }
     
-		if (last_pitch > pitch)
-		{
-		delta_pitch = -1;
-		pitch_error = last_pitch - pitch;
-		}
+    if (last_pitch > pitch)
+    {
+    delta_pitch = -1;
+    pitch_error = last_pitch - pitch;
+    }
     
-		if (last_roll < angle) 
-			delta_roll  = 1;
-		if (last_roll > angle) 
-			delta_roll  = -1;
+    if (last_roll < angle) 
+      delta_roll  = 1;
+    if (last_roll > angle) 
+      delta_roll  = -1;
     
-		if (delta_roll == 0) 
-		{
-		if (pitch_error > 1) delta_pitch *= 2;
-    		}
+    if (delta_roll == 0) 
+    {
+    if (pitch_error > 1) 
+      delta_pitch *= 2;
+    }
     
-	drawHorizon(last_roll + delta_roll, last_pitch + delta_pitch);
-	drawInfo();
-	}
+  drawHorizon(last_roll + delta_roll, last_pitch + delta_pitch);
+  drawInfo();
+  }
 }
 
 void drawHorizon(int angle, int pitch)
@@ -108,133 +112,135 @@ void drawHorizon(int angle, int pitch)
 float sx = cos(angle * DEG2RAD);
 float sy = sin(angle * DEG2RAD);
 
-int16_t x0 = sx * HOR + 100;
+int16_t x0 = sx * HOR;
 int16_t y0 = sy * HOR;
 
-	if ((angle != last_roll) && ((abs(angle) > 35)  || (pitch != last_pitch)))
-	{
-	tft.drawLine(XC - x0, YC - y0 - 3 + pitch, XC + x0, YC + y0 - 3 + pitch, BLUE);
-	tft.drawLine(XC - x0, YC - y0 + 3 + pitch, XC + x0, YC + y0 + 3 + pitch, BORDO);
-	tft.drawLine(XC - x0, YC - y0 - 4 + pitch, XC + x0, YC + y0 - 4 + pitch, BLUE);
-	tft.drawLine(XC - x0, YC - y0 + 4 + pitch, XC + x0, YC + y0 + 4 + pitch, BORDO);
-	}
+  if ((angle != last_roll) && ((abs(angle) > 35)  || (pitch != last_pitch)))
+  {
+  tft.drawLine(0, YC - y0 - 3 + pitch, 320, YC + y0 - 3 + pitch, BLUE);
+  tft.drawLine(0, YC - y0 + 3 + pitch, 320, YC + y0 + 3 + pitch, BORDO);
+  tft.drawLine(0, YC - y0 - 4 + pitch, 320, YC + y0 - 4 + pitch, BLUE);
+  tft.drawLine(0, YC - y0 + 4 + pitch, 320, YC + y0 + 4 + pitch, BORDO);
+  //tft.drawLine(XC - x0, YC - y0 - 5 + pitch, XC + x0, YC + y0 - 5 + pitch, BORDO);
+  //tft.drawLine(XC - x0, YC - y0 + 5 + pitch, XC + x0, YC + y0 + 5 + pitch, BLUE);
+  }
 
-	tft.drawLine(XC - x0, YC - y0 - 2 + pitch, XC + x0, YC + y0 - 2 + pitch, BLUE);
-	tft.drawLine(XC - x0, YC - y0 + 2 + pitch, XC + x0, YC + y0 + 2 + pitch, BORDO);
+  tft.drawLine(0, YC - y0 - 2 + pitch, 320, YC + y0 - 2 + pitch, BLUE);
+  tft.drawLine(0, YC - y0 + 2 + pitch, 320, YC + y0 + 2 + pitch, BORDO);
 
-	tft.drawLine(XC - x0, YC - y0 - 1 + pitch, XC + x0, YC + y0 - 1 + pitch, BLUE);
-	tft.drawLine(XC - x0, YC - y0 + 1 + pitch, XC + x0, YC + y0 + 1 + pitch, BORDO);
+  tft.drawLine(0, YC - y0 - 1 + pitch, 320, YC + y0 - 1 + pitch, BLUE);
+  tft.drawLine(0, YC - y0 + 1 + pitch, 320, YC + y0 + 1 + pitch, BORDO);
 
-	tft.drawLine(XC - x0, YC - y0 + pitch,   XC + x0, YC + y0 + pitch,   WHITE);
+  tft.drawLine(0, YC - y0 + pitch, 320, YC + y0 + pitch,   WHITE);
 
-	last_roll = angle;
-	last_pitch = pitch;  
+  last_roll = angle;
+  last_pitch = pitch;  
 }
 
 void drawInfo(void)
 {
-	//Rysowanie linii pomocniczych
-	tft.fillRect(160 - 1, 120 - 1, 3, 3, RED);
-	tft.drawFastHLine(160 - 30,   120, 24, RED);
-	tft.drawFastHLine(160 + 30 - 24, 120, 24, RED);
-	tft.drawFastVLine(160 - 30 + 24, 120, 3, RED);
-	tft.drawFastVLine(160 + 30 - 24, 120, 3, RED);
+  //Rysowanie linii pomocniczych
+  tft.fillRect(160 - 1, 120 - 1, 3, 3, RED);
+  tft.drawFastHLine(160 - 30,   120, 24, RED);
+  tft.drawFastHLine(160 + 30 - 24, 120, 24, RED);
+  tft.drawFastVLine(160 - 30 + 24, 120, 3, RED);
+  tft.drawFastVLine(160 + 30 - 24, 120, 3, RED);
 
-	tft.drawFastHLine(160 - 12,   120 - 40, 24, WHITE);
-	tft.drawFastHLine(160 -  6,   120 - 30, 12, WHITE);
-	tft.drawFastHLine(160 - 12,   120 - 20, 24, WHITE);
-	tft.drawFastHLine(160 -  6,   120 - 10, 12, WHITE);
+  tft.drawFastHLine(160 - 12,   120 - 40, 24, WHITE);
+  tft.drawFastHLine(160 -  6,   120 - 30, 12, WHITE);
+  tft.drawFastHLine(160 - 12,   120 - 20, 24, WHITE);
+  tft.drawFastHLine(160 -  6,   120 - 10, 12, WHITE);
 
-	tft.drawFastHLine(160 -  6,   120 + 10, 12, WHITE);
-	tft.drawFastHLine(160 - 12,   120 + 20, 24, WHITE);
-	tft.drawFastHLine(160 -  6,   120 + 30, 12, WHITE);
-	tft.drawFastHLine(160 - 12,   120 + 40, 24, WHITE);
+  tft.drawFastHLine(160 -  6,   120 + 10, 12, WHITE);
+  tft.drawFastHLine(160 - 12,   120 + 20, 24, WHITE);
+  tft.drawFastHLine(160 -  6,   120 + 30, 12, WHITE);
+  tft.drawFastHLine(160 - 12,   120 + 40, 24, WHITE);
 
-	tft.setTextColor(WHITE);
-	tft.cursorToXY(160 - 12 - 13, 120 - 20 - 3);
-	tft.print("10");
-	tft.cursorToXY(160 + 12 + 1, 120 - 20 - 3);
-	tft.print("10");
-	tft.cursorToXY(160 - 12 - 13, 120 + 20 - 3);
-	tft.print("10");
-	tft.cursorToXY(160 + 12 + 1, 120 + 20 - 3);
-	tft.print("10");
+  tft.setTextColor(WHITE);
+  tft.setTextArea(160 - 12 - 13, 120 - 20 - 3, 10, 10);
+  tft.print("10");
+  tft.setTextArea(160 + 12 + 1, 120 - 20 - 3, 10, 10);
+  tft.print("10");
+  tft.setTextArea(160 - 12 - 13, 120 + 20 - 3, 10, 10);
+  tft.print("10");
+  tft.setTextArea(160 + 12 + 1, 120 + 20 - 3, 10, 10);
+  tft.print("10");
 
-	tft.cursorToXY(160 - 12 - 13, 120 - 40 - 3);
-	tft.print("20");
-	tft.cursorToXY(160 + 12 + 1, 120 - 40 - 3);
-	tft.print("20");
-	tft.cursorToXY(160 - 12 - 13, 120 + 40 - 3);
-	tft.print("20");
-	tft.cursorToXY(160 + 12 + 1, 120 + 40 - 3);
-	tft.print("20");
+  tft.setTextArea(160 - 12 - 13, 120 - 40 - 3, 10, 10);
+  tft.print("20");
+  tft.setTextArea(160 + 12 + 1, 120 - 40 - 3, 10, 10);
+  tft.print("20");
+  tft.setTextArea(160 - 12 - 13, 120 + 40 - 3, 10, 10);
+  tft.print("20");
+  tft.setTextArea(160 + 12 + 1, 120 + 40 - 3, 10, 10);
+  tft.print("20");
 
-	// Wyświetlanie aktualnej wartości przechyłu
-	tft.setTextColor(YELLOW, BORDO);
-	tft.setTextArea(120, 180, 50, 10);
-	//tft.printAt(last_roll, 120, 180);
+  // Wyświetlanie aktualnej wartości przechyłu
+  tft.setTextColor(YELLOW, BORDO);
+  tft.setTextArea(130, 210, 50, 20);
+  tft.clearTextArea(BORDO);            
+  tft.printAligned(String(last_roll, DEC), gTextAlignMiddleCenter);
 }
 
-int angleGenerator(int maxAngle)	//Generator przechyłu - do testu
+int angleGenerator(int maxAngle)  //Generator przechyłu - do testu
 {
-  	// Generowanie kątu przechyłu z ustaloną rozdzielnością w celach testowych
-	delta++; 
-	if (delta >= 360) 
-		test_angle = 0;
- 	test_angle = (maxAngle + 1) * sin((delta) * DEG2RAD);
+    // Generowanie kątu przechyłu z ustaloną rozdzielnością w celach testowych
+  delta++; 
+  if (delta >= 360) 
+    test_angle = 0;
+  test_angle = (maxAngle + 1) * sin((delta) * DEG2RAD);
 
-	// Ograniczenie maksymalnego kątu przechyłu
-	if (test_angle >  maxAngle)
-		test_angle =  maxAngle;
-	if (test_angle < -maxAngle)
-		test_angle = -maxAngle;
-	
+  // Ograniczenie maksymalnego kątu przechyłu
+  if (test_angle >  maxAngle)
+    test_angle =  maxAngle;
+  if (test_angle < -maxAngle)
+    test_angle = -maxAngle;
+  
   return test_angle;
 }
 
-void testRoll(void)	//Obrót - przebieg testowy
+void testRoll(void) //Obrót - przebieg testowy
 {
-	tft.setTextColor(YELLOW, BLUE);
-	tft.setTextArea(120, 180, 50, 10);
-	tft.printAligned("Roll test", gTextAlignMiddleCenter);
+  tft.setTextColor(YELLOW, BORDO);
+  tft.setTextArea(120, 180, 50, 10);
+  tft.printAligned("Roll test", gTextAlignMiddleCenter);
 
-	for (int a = 0; a < 360; a++)
-	{
-	//delay(REDRAW_DELAY / 2);
-	updateHorizon(angleGenerator(50), 0);
-	}
-	
-	tft.setTextColor(YELLOW, BLUE);
-	tft.setTextArea(120, 180, 50, 10);
-	tft.printAligned("                ", gTextAlignMiddleCenter);
+  for (int a = 0; a < 360; a++)
+  {
+  //delay(REDRAW_DELAY / 2);
+  updateHorizon(angleGenerator(50), 0);
+  }
+  
+  tft.setTextColor(YELLOW, BORDO);
+  tft.setTextArea(120, 180, 50, 10);
+  tft.printAligned("                ", gTextAlignMiddleCenter);
 }
 
-void testPitch(void)				//Nachylenie - przebieg testowy
+void testPitch(void)        //Nachylenie - przebieg testowy
 {
-	tft.setTextColor(YELLOW, BLUE);
-	tft.setTextArea(120, 180, 50, 10);            
-	tft.printAligned("Pitch test", gTextAlignMiddleCenter);
+  tft.setTextColor(YELLOW, BORDO);
+  tft.setTextArea(120, 180, 50, 10);            
+  tft.printAligned("Pitch test", gTextAlignMiddleCenter);
 
-	for (int p = 0; p > -50; p--) 
-	{
-	delay(REDRAW_DELAY / 2);
-	updateHorizon(0, p);
-	}
+  for (int p = 0; p > -50; p--) 
+  {
+  delay(REDRAW_DELAY / 2);
+  updateHorizon(0, p);
+  }
 
-	for (int p = -50; p < 50; p++) 
-	{
-	delay(REDRAW_DELAY / 2);
-	updateHorizon(0, p);
-	}
+  for (int p = -50; p < 50; p++) 
+  {
+  delay(REDRAW_DELAY / 2);
+  updateHorizon(0, p);
+  }
 
-	for (int p = 50; p > 0; p--)
-	{
-	delay(REDRAW_DELAY / 2);
-	updateHorizon(0, p);
-	}
+  for (int p = 50; p > 0; p--)
+  {
+  delay(REDRAW_DELAY / 2);
+  updateHorizon(0, p);
+  }
 
-	tft.setTextColor(YELLOW, BLUE);
-	tft.setTextArea(120, 180, 50, 10);            
-	tft.printAligned("                ", gTextAlignMiddleCenter);
+  tft.setTextColor(YELLOW, BORDO);
+  tft.setTextArea(120, 180, 50, 10);            
+  tft.printAligned("                ", gTextAlignMiddleCenter);
 }
-
